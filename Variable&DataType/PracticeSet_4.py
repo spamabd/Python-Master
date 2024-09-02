20d5142fb292869fa8b5f9de928fe0b98f11d2a9
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-09-02 01:42:06 -0700

    Create PracticeSet_4.py

---
+++ Variable&DataType/PracticeSet_4.py
@@ -0,0 +1,6 @@
+# write a print to input 2 integer a & b print true if a is greater than or eqaul to b else print false
+
+a = int(input("enter their value"))
+b = int(input("enter their value"))
+if(a>=b): print(True)
+else: print(False)
