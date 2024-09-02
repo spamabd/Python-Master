fdb9b7543a47bc9f2cbc61680b7152edfb6a3349
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-09-02 01:41:15 -0700

    Create PracticeSet_3.py

---
+++ Variable&DataType/PracticeSet_3.py
@@ -0,0 +1,6 @@
+# write a program to input 2 floating point nuber & print their average
+
+val1 = float(input("enter their number"))
+val2 = float(input("enter their number"))
+average = (val1+val2)/2
+print(average)
