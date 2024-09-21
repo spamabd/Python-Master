d9ffa8cb2f75fcaf6abcd9ca4c123200d0e732f2
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-09-21 00:43:18 -0700

    Create PracticeSet_5.py

---
+++ String&ConditionSTatement/PracticeSet_5.py
@@ -0,0 +1,12 @@
+# write a program to find the greatest of 3 enterd by the user
+
+num1= int(input("enter the number"))
+num2= int(input("enter the number"))
+num3= int(input("enter the number"))
+
+if(num1>num2):
+    print("this is greatest number", num1 )
+elif(num2>num3):
+      print("this is greatest number" , num2)
+elif(num3>num1):
+      print("this is greatest number", num3 )  
