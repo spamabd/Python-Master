e73d0bd7edcee3162e276f2d7d2e76f9e827c37d
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-09-21 00:43:02 -0700

    Create PracticeSet_4.py

---
+++ String&ConditionSTatement/PracticeSet_4.py
@@ -0,0 +1,9 @@
+# Write a program to check if a number entered by the user is odd or even 
+
+num1 = int(input("enter the number "))
+
+if(num1 % 2 == 0):
+    print("this number is even")
+else:
+    print("this number is odd")
+
