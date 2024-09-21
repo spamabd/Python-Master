72bcc6b3ea100d3dc6e07da86d081306d6ab74f5
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-09-21 00:43:39 -0700

    Create PracticeSet_6.py

---
+++ String&ConditionSTatement/PracticeSet_6.py
@@ -0,0 +1,8 @@
+# write a program to check a number is multiply by 7 or not 
+
+num1= int(input("Enter the number"))
+
+if(num1 % 7 == 0):
+    print( num1 ,"This number is multiple by 7")
+else:
+    print(num1 , "This number is not multiple by 7")
