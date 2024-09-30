b7ed98a012a0b392c9997b328422b0a8901a81ea
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-09-30 04:49:50 -0700

    Create PracticeSet_2.py

---
+++ Dictionary&Set/PracticeSet_2.py
@@ -0,0 +1,11 @@
+
+
+marks = {}
+subject = int(input("English marks"))
+marks.update({"english" : subject})
+subject = int(input("Urdu marks"))
+marks.update({"Urdu" : subject})
+subject = int(input("Physic marks"))
+marks.update({"Physic" : subject})
+
+print(marks)
