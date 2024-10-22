9ce342a062bffd36aeba4055687070f53bf392ab
Abdul Raheem <143559506+spamabd@users.noreply.github.com>
2024-10-22 12:24:42 -0700

    Create PracticeSet_1.py

---
+++ Fuction&Recursion/PracticeSet_1.py
@@ -0,0 +1,10 @@
+# Write a function to print the lenght of a list (list is the paramet
+
+cities = ["karachi", "lahore", "islamabad", "quetta", ]
+
+def print_lenght(cities):
+    print(len(cities))
+    return
+
+print_lenght(cities)
+
